7bfeab6d483d13923044c886a51687dc32121c61
Dylan <dylan.cobean@gmail.com>
2021-03-15 13:58:06 -0700

    Setup | Basic Mapbox Map Running

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,12 @@
-// Add your scripts here
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ";
+
+// const map =
+new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-110.92, 32.22], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
