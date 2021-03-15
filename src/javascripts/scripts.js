83f8ee40ef7f64caec88ebddb45ce231f3fff0c1
Dylan <dylan.cobean@gmail.com>
2021-03-15 15:40:10 -0700

    Added Zoom Controls | Need it for people who don't have a mousewheel for zooming

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -3,10 +3,16 @@ import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ";
 
-// const map =
-new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/dylanc/ckm60iiq30tz817p3whz91c5j", // style URL
   center: [-110.92, 32.22], // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
+
+var nav = new mapboxgl.NavigationControl({
+  showCompass: true,
+  showZoom: true,
+  visualizePitch: false,
+});
+map.addControl(nav, 'top-right');
